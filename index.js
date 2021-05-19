6da882aaf1c4e13b97f5ef6cbddf39c2f15d5a16
Daniel Mueller <danielpetermueller@gmail.com>
2021-05-19 19:41:13 -0400

    commit tree f31aa90bbac03b69b9156397ece5130352df7796

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter(function(person){
+        return person.toLowerCase() === name.toLowerCase()
+    });
+    return newDrivers;
+}
+function fuzzyMatch(drivers, name){
+    const fuzzyDrivers = drivers.filter(function(person){
+        return person.slice(0,1) === name.slice(0,1);
+    });
+    return fuzzyDrivers;
+}
+function matchName(drivers, name){
+    const nameMatch = drivers.filter(function(person){
+        return person === name;
+    });
+    return nameMatch;
+}
